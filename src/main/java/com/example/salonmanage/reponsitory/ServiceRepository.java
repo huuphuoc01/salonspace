d62bc33055f3923a719135fbd800061f9ccdf77e
phuoc <phuochvhue2001@gmail.com>
2023-08-03 23:47:00 +0700

    add api search service. fix booking

--- src/main/java/com/example/salonmanage/reponsitory/ServiceRepository.java
+++ src/main/java/com/example/salonmanage/reponsitory/ServiceRepository.java
@@ -3,6 +3,7 @@ package com.example.salonmanage.reponsitory;
 import com.example.salonmanage.Entities.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,9 @@ import java.util.List;
 public interface ServiceRepository  extends JpaRepository<Service, Integer> {
     @Query("SELECT b FROM Service b WHERE b.status != 3")
     List<Service> findAllWithNotRemove();
+
+    @Query("SELECT b FROM Service b WHERE b.status != 3 AND b.name LIKE %:serviceName%")
+    List<Service> findAllWithNotRemoveByName(@Param("serviceName") String serviceName);
+
     Service findServiceById(Integer id);
 }
