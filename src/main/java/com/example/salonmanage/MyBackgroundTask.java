c9f0778d3b8796772a0543d7b01aed7695e15c09
luongna <anhluong7101@gmail.com>
2023-08-24 02:17:58 +0700

    event

--- src/main/java/com/example/salonmanage/MyBackgroundTask.java
+++ src/main/java/com/example/salonmanage/MyBackgroundTask.java
@@ -1,14 +1,16 @@
 package com.example.salonmanage;
 
+import com.example.salonmanage.reponsitory.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyBackgroundTask {
 
-//    @Scheduled(fixedRate = 10000) // Runs every 1 second
-//    public void executeTask() {
-//        // Your task logic here
-//        System.out.println("Background task is running...");
-//    }
+    @Autowired private BookingRepository bookingRepository;
+    @Scheduled(fixedRate = 1800000) // Runs every 1 second
+    public void executeTask() {
+        bookingRepository.checkStatus();
+    }
 }
