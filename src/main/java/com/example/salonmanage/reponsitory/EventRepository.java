1175eee66d014c59605ac96e70282231de7abe96
Hongphucqh1234 <hoangphucqh1234@gmail.com>
2023-08-24 17:35:12 +0700

    fix  payment

--- src/main/java/com/example/salonmanage/reponsitory/EventRepository.java
+++ src/main/java/com/example/salonmanage/reponsitory/EventRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<event,Integer> {
     @Query(value = "select top(1) * from event where status = 0",nativeQuery = true)
     event showevent();
-    event getByDate(String date);
+   List<event> findByDate(String date);
 
     @Query(value = "select  * from event where status != 3",nativeQuery = true)
     List<event> getList();
